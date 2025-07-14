dd42af893a55f740abd18876dd3656b71f465a9d
Kristian Pacheco <kristianpachecar18@gmail.com>
2025-07-14 23:01:58 +0000

    Added backend url

--- src/front/pages/SignUp.jsx
+++ src/front/pages/SignUp.jsx
@@ -1,6 +1,7 @@
 import { Link, useNavigate } from "react-router-dom";
 import React, { useState } from "react";
 import useGlobalReducer from "../hooks/useGlobalReducer";
+const backendUrl = import.meta.env.VITE_BACKEND_URL;
 
 export const SignUp = () => {
     const [formData, setFormData] = useState({
